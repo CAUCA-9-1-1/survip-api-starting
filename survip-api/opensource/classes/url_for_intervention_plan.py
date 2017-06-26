aabc40c98a8dffe60765a13f3d998a5be037069d
michael.jolin <michael.jolin@cauca.ca>
2017-06-26 13:52:52 -0400

    (feat) Add resturls for "constructiontype"

--- survip-api/opensource/classes/url_for_intervention_plan.py
+++ survip-api/opensource/classes/url_for_intervention_plan.py
@@ -6,3 +6,7 @@ class UrlForInterventionPlan(BaseApi):
 	@cherrypy.expose
 	def alarmpaneltype(self, *args, **kwargs):
 		return self.call_method('AlarmPanelType', self.get_argument(args, kwargs))
+
+	@cherrypy.expose
+	def constructiontype(self, *args, **kwargs):
+		return self.call_method('ConstructionType', self.get_argument(args, kwargs))
